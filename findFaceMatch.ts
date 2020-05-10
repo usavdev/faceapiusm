0457931506364be3024fa958fda5fb62c23e1b90
Усманов Алексей Викторович <AUsmanov@bis.ru>
2020-05-10 11:48:01 +0300

    little fix

--- findFaceMatch.ts
+++ findFaceMatch.ts
@@ -2,43 +2,56 @@ import * as faceapi from 'face-api.js'
 
 import { canvas, faceDetectionNet, faceDetectionOptions, saveFile } from './commons'
 
-const fs = require('fs')
-const fsPromises = fs.promises
-const weightsPath = './weights'
+export module findFaceMatch {
+    const fs = require('fs')
+    const fsPromises = fs.promises
+    const weightsPath = './weights'
 
-const superName = 'facebase'
-const impJson = `./${superName}.json`;
+    const superName = 'facebase'
+    const impJson = `./${superName}.json`;
 
-async function loadJson() {
-    try {
-        return fsPromises.readFile(impJson);
-    } catch (err) {
-        console.error('Error occured while reading directory!', err);
+    async function loadJson() {
+        try {
+            return fsPromises.readFile(impJson);
+        } catch (err) {
+            console.error('Error occured while reading directory!', err);
+        }
     }
+    
+    async function run(imgPath) {
+
+        await faceDetectionNet.loadFromDisk(weightsPath)
+        await faceapi.nets.faceLandmark68Net.loadFromDisk(weightsPath)
+        await faceapi.nets.faceRecognitionNet.loadFromDisk(weightsPath)
+        await faceapi.nets.ageGenderNet.loadFromDisk(weightsPath)  
+
+        const faceMatcherJSON = JSON.parse(await loadJson())
+        const faceMatcher = faceapi.FaceMatcher.fromJSON(faceMatcherJSON)
+
+        const img = await canvas.loadImage(imgPath)
+        const imgOutPath = imgPath.replace(/.jpg/i, '_out.jpg')
+
+        const singleResult = await faceapi
+            .detectSingleFace(img, faceDetectionOptions)
+            .withFaceLandmarks()
+            .withFaceDescriptor()
+            
+        if (singleResult) {
+            // console.log(faceMatcher.labeledDescriptors)
+            const bestMatch = faceMatcher.matchDescriptor(singleResult.descriptor)
+            const queryDrawBoxes = new faceapi.draw.DrawBox(singleResult.detection.box, { label: bestMatch.toString() })
+            const outQuery = faceapi.createCanvasFromMedia(img)
+            queryDrawBoxes.draw(outQuery)            
+            fs.writeFileSync(imgOutPath, (outQuery as any).toBuffer('image/jpeg'))            
+            return {status: 'ok', bestmatch: bestMatch, outpath: imgOutPath}
+        } else {
+            return {status: 'error', desc:'Can`t find face in this image'}
+        }       
+    }
+
+    export async function runner(imgPath = 'test.jpg') {
+        return await run(imgPath)
+    }
+
 }
-  
-async function run() {
 
-    await faceDetectionNet.loadFromDisk(weightsPath)
-    await faceapi.nets.faceLandmark68Net.loadFromDisk(weightsPath)
-    await faceapi.nets.faceRecognitionNet.loadFromDisk(weightsPath)
-    await faceapi.nets.ageGenderNet.loadFromDisk(weightsPath)  
-
-    const faceMatcherJSON = JSON.parse(await loadJson())
-    const faceMatcher = faceapi.FaceMatcher.fromJSON(faceMatcherJSON)
-
-    const img = await canvas.loadImage(`test4.jpg`)
-
-    const singleResult = await faceapi
-        .detectSingleFace(img, faceDetectionOptions)
-        .withFaceLandmarks()
-        .withFaceDescriptor()
-        
-    if (singleResult) {
-        console.log(faceMatcher.labeledDescriptors)
-        const bestMatch = faceMatcher.matchDescriptor(singleResult.descriptor)
-        console.log(bestMatch.toString())
-    }        
-}
-
-run()
